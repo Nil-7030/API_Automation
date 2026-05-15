e20a868c41e84cb43f37b63b17bbe6e7aa14d6ac
Nilesh Rajendra Behare <nileshrajendrabehare@Nilesh-Rajendra-Behare-B707s-MacBook-Air.local>
2026-05-15 12:42:40 +0530

    updated code

--- restassured-project/src/test/java/com/myproject/base/BaseClass.java
+++ restassured-project/src/test/java/com/myproject/base/BaseClass.java
@@ -9,74 +9,73 @@ import java.util.Map;
 
 import org.testng.annotations.BeforeMethod;
 
-
 import com.myproject.tests.UserLogin;
 import com.myproject.utils.ConfigReader;
 
 public class BaseClass {
 
-    protected RequestSpecification request;
-    public static String token;
+        protected RequestSpecification request;
+        public static String token;
 
-    @BeforeMethod
-    public void setup() {
+        @BeforeMethod
+        public void setup() {
 
-        request = given()
-                .baseUri(ConfigReader.get("BASE_URL"))
-                .header("Content-Type", "application/json");
+                request = given()
+                                .baseUri(ConfigReader.get("BASE_URL"))
+                                .header("Content-Type", "application/json");
 
-        token = new UserLogin().userLogin();
-        System.out.println("Generated Token: " + token);
+                token = new UserLogin().userLogin();
+                System.out.println("Generated Token: " + token);
 
-        request = given()
-                .baseUri(ConfigReader.get("BASE_URL"))
+                request = given()
+                                .baseUri(ConfigReader.get("BASE_URL"))
 
-                .header("Authorization", "Bearer " + token)
-                .header("Content-Type", "application/json");
-    }
+                                .header("Authorization", "Bearer " + token)
+                                .header("Content-Type", "application/json");
+        }
 
-     public Response getWithPathParam(String endpoint, Object pathParam) {
-        return given()
-                .header("Content-Type", "application/json")
-                .pathParam("petId", pathParam)
-                .when()
-                .get(endpoint);
-    }
+        public Response getWithPathParam(String endpoint, Object pathParam) {
+                return given()
+                                .header("Content-Type", "application/json")
+                                .pathParam("petId", pathParam)
+                                .when()
+                                .get(endpoint);
+        }
 
-    public Response postWithFormData(String endpoint,
-            Object pathParam,
-            Map<String, Object> formData) {
+        public Response postWithFormData(String endpoint,
+                        Object pathParam,
+                        Map<String, Object> formData) {
 
-        return given()
-                .header("Content-Type", "application/json")
-                .pathParam("petId", pathParam)
-                .formParams(formData)
-                .when()
-                .post(endpoint);
-    }
+                return given()
+                                .header("Content-Type", "application/json")
+                                .pathParam("petId", pathParam)
+                                .formParams(formData)
+                                .when()
+                                .post(endpoint);
+        }
 
-    public Response postRequest(String endpoint, Map<String, Object> body) {
-        return given()
-                .header("Content-Type", "application/json")
-                .body(body)
-                .when()
-                .post(endpoint);
-    }
+        public Response postRequest(String endpoint, Map<String, Object> body) {
+                return given()
+                                .header("Content-Type", "application/json")
+                                .body(body)
+                                .when()
+                                .post(endpoint);
+        }
 
-    public Response putRequest(String endpoint, Map<String, Object> body) {
-        return given()
-                .header("Content-Type", "application/json")
-                .body(body)
-                .when()
-                .put(endpoint);
-    }
+        public Response putRequest(String endpoint, Map<String, Object> body) {
+                return given()
+                                .header("Content-Type", "application/json")
+                                .body(body)
+                                .when()
+                                .put(endpoint);
+        }
 
-    public Response deleteWithPathParam(String endpoint, Object pathParam) {
-        return given()
-                .header("Content-Type", "application/json")
-                .pathParam("petId", pathParam)
-                .when()
-                .delete(endpoint);
-    } 
+        public Response deleteWithPathParam(String endpoint, Object pathParam) {
+                return given()
+                                .header("Content-Type", "application/json")
+                                .pathParam("petId", pathParam)
+                                .when()
+                                .delete(endpoint);
+        }
 
 }
